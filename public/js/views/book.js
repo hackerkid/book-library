a9ea6b59d390f5b06a92ce36f9249c051d0c2768
Vishnu Ks <hackerkid@vishnuks.com>
2015-12-25 21:41:23 +0530

    first commit

---
+++ public/js/views/book.js
@@ -0,0 +1,30 @@
+
+var app = app || {};
+
+app.BookView = Backbone.View.extend({
+    tagName: 'div',
+    className: 'bookContainer',
+    template: _.template( $( '#bookTemplate' ).html() ),
+
+    events: {
+        'click .delete': 'deleteBook'
+    },
+
+    deleteBook: function() {
+        //Delete model
+        this.model.destroy();
+
+        //Delete view
+        this.remove();
+    },
+    render: function() {
+        this.$el.html( this.template( this.model.attributes ) );
+        return this;
+    },
+
+    initialize: function() {
+    	console.log(this.model.attributes);
+    },
+
+});
+
