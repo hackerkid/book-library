08812e742e7f32b4fd70ac2efbc85ab7d618decf
Vishnu Ks <hackerkid@vishnuks.com>
2015-12-26 17:36:47 +0530

    code merged from org repo

--- public/js/views/book.js
+++ public/js/views/book.js
@@ -1,30 +1,29 @@
-
 var app = app || {};
 
 app.BookView = Backbone.View.extend({
-    tagName: 'div',
-    className: 'bookContainer',
-    template: _.template( $( '#bookTemplate' ).html() ),
+	tagName: 'div',
+	className: 'bookContainer',
+	template: $( '#bookTemplate' ).html(),
 
-    events: {
-        'click .delete': 'deleteBook'
-    },
+	events: {
+		'click .delete': 'deleteBook'
+	},
 
-    deleteBook: function() {
-        //Delete model
-        this.model.destroy();
+	deleteBook: function() {
+		//Delete model
+		this.model.destroy();
 
-        //Delete view
-        this.remove();
-    },
-    render: function() {
-        this.$el.html( this.template( this.model.attributes ) );
-        return this;
-    },
+		//Delete view
+		this.remove();
+	},
 
-    initialize: function() {
-    	console.log(this.model.attributes);
-    },
+	render: function() {
+		//tmpl is a function that takes a JSON object and returns html
+		var tmpl = _.template( this.template );
 
+		//this.el is what we defined in tagName. use $el to get access to jQuery html() function
+		this.$el.html( tmpl( this.model.toJSON() ) );
+
+		return this;
+	}
 });
-
--- server.js
+++ server.js
@@ -3,15 +3,13 @@ var application_root = __dirname,
     bodyParser = require('body-parser'), //Parser for reading request body
     path = require( 'path' ), //Utilities for dealing with file paths
     mongoose = require( 'mongoose' ); //MongoDB integration
+	methodOverride = require('method-override');
+
 
 //Create server
 var app = express();
 
-//Where to serve static content
-app.use( express.static( path.join( application_root, 'site') ) );
-app.use(bodyParser());
 
-//Start server
 var port = 4711;
 
 app.listen( port, function() {
@@ -22,29 +20,103 @@ app.listen( port, function() {
 mongoose.connect( 'mongodb://localhost/library_database' );
 
 //Schemas
+
+var Keywords = new mongoose.Schema({
+    keyword: String
+});
+
 var Book = new mongoose.Schema({
     title: String,
     author: String,
-    releaseDate: Date
+    releaseDate: Date,
+    keywords: [ Keywords ]   
+
 });
 
 //Models
 var BookModel = mongoose.model( 'Book', Book );
 
-// Configure server
-app.configure( function() {
-    //parses request body and populates request.body
-    app.use( express.bodyParser() );
 
-    //checks request.body for HTTP method overrides
-    app.use( express.methodOverride() );
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
-    //perform route lookup based on url and HTTP method
-    app.use( app.router );
+app.get( '/api', function( request, response ) {
+    response.send( 'Library API is running' );
+})
+app.get( '/', function( request, response ) {
+    response.sendFile(__dirname + '/public/index.html');
+})
 
-    //Where to serve static content
-    app.use( express.static( path.join( application_root, 'site') ) );
+app.get( '/api/books', function( request, response ) {
+    return BookModel.find( function( err, books ) {
+        if( !err ) {
+            return response.send( books );
+        } else {
+            return console.log( err );
+        }
+    });
+});
 
-    //Show all errors in development
-    app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+
+//Get a single book by id
+app.get( '/api/books/:id', function( request, response ) {
+    return BookModel.findById( request.params.id, function( err, book ) {
+        if( !err ) {
+            return response.send( book );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+
+app.post( '/api/books', function( request, response ) {
+    var book = new BookModel({
+        title: request.body.title,
+        author: request.body.author,
+        releaseDate: request.body.releaseDate,
+        keywords: request.body.keywords       // NEW
+    });
+    book.save( function( err ) {
+        if( !err ) {
+            console.log( 'created' );
+            return response.send( book );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+
+//Update a book
+app.put( '/api/books/:id', function( request, response ) {
+    console.log( 'Updating book ' + request.body.title );
+    return BookModel.findById( request.params.id, function( err, book ) {
+        book.title = request.body.title;
+        book.author = request.body.author;
+        book.releaseDate = request.body.releaseDate;
+        book.keywords = request.body.keywords; // NEW
+
+        return book.save( function( err ) {
+            if( !err ) {
+                console.log( 'book updated' );
+            } else {
+                console.log( err );
+            }
+            return response.send( book );
+        });
+    });
+});
+app.delete( '/api/books/:id', function( request, response ) {
+    console.log( 'Deleting book with id: ' + request.params.id );
+    return BookModel.findById( request.params.id, function( err, book ) {
+        return book.remove( function( err ) {
+            if( !err ) {
+                console.log( 'Book removed' );
+                return response.send( '' );
+            } else {
+                console.log( err );
+            }
+        });
+    });
 });
